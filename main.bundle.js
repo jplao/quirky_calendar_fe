9165742f9f05b26ff23a9ecfc2e716092c01f2bc
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-25 12:43:11 -0700

    Add functionality to display current holidays

--- main.bundle.js
+++ main.bundle.js
@@ -44,8 +44,54 @@
 /* 0 */
 /***/ (function(module, exports) {
 
+	'use strict';
+
 	// This file is in the entry point in your webpack config.
-	"use strict";
+	var date = new Date();
+	var dd = date.getDate();
+	var mm = date.getMonth() + 1; //January is 0!
+	var yyyy = date.getFullYear();
+
+	if (dd < 10) {
+	  dd = '0' + dd;
+	}
+
+	if (mm < 10) {
+	  mm = '0' + mm;
+	}
+
+	var date = yyyy + '-' + mm + '-' + dd;
+
+	function getHolidays() {
+	  var _this = this;
+
+	  var url = 'https://quirkycalendar.herokuapp.com/api/v1/holidays?date=' + date;
+	  fetch(url).then(function (response) {
+	    return response.json();
+	  }).then(function (json_response) {
+	    _this.showHolidays(json_response);
+	  });
+	}
+
+	function showHolidays(json_response) {
+	  var holidayArray = json_response;
+	  var holiday_list = document.getElementById("holidays");
+	  holidayArray['data'].forEach(function (holiday) {
+	    var name = holiday['attributes']['name'];
+	    var holiday_data = '<h3>' + name + '</h3><br>';
+	    appendData(holiday_data, "holidays");
+	  });
+	}
+
+	function appendData(data, parent_id) {
+	  var newDiv = document.createElement('div');
+	  newDiv.className = 'holiday';
+	  newDiv.innerHTML = data;
+	  var parent_element = document.getElementById(parent_id);
+	  parent_element.appendChild(newDiv);
+	}
+
+	$(document).on("load", getHolidays());
 
 /***/ })
 /******/ ]);
